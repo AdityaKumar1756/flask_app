022f195b7941c2ab1f06090947e865a3932b8345
Aditya Kumar <aditya1756in@gmail.com>
2026-08-08 09:12:02 -0400

    changing the postgres in db_url to postgresql

--- app.py
+++ app.py
@@ -9,10 +9,17 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     
-    app.config["SECRET KEY"] = os.environ.get("SECRET_KEY", "dev-only-insecure-key")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-        "DATABASE_URL", "sqlite:///blog.db"
-    )
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev-only-insecure-key")
+    
+    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    #     "DATABASE_URL", "sqlite:///blog.db"
+    # )
+    
+    db_url = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    app.config("SQLALCHEMY_DATABASE_URI") = db_url  
+    
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
     db.init_app(app)
